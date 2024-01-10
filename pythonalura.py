a808284abd043ed31a09ec4a6058a872db9693b4
Elias <eliassiqueira584@gmail.com>
2024-01-10 06:54:22 -0300

    Update pythonalura.py

--- pythonalura.py
+++ pythonalura.py
@@ -1,20 +1,29 @@
-print("***********************************")
-print("Bem vindo, ao jogo da adivinhação!")
-print("***********************************")
+
+print("************************************")
+print("*Bem vindo, ao jogo da adivinhação!*")
+print("************************************")
 
 numero_secreto = 80
+tentativa = 3
+rodada = 1
 
+while(rodada <= tentativa):
+   print("Rodada {} de {}".format(rodada, tentativa))
+   chute = int (input("Digite seu palpite:"))
+   print("Você digitou",chute)
 
-chute = int (input("Digite seu palpite:"))
+   acertou = numero_secreto == chute
+   maior   = chute > numero_secreto
+   menor   = chute < numero_secreto
 
-
-print("Você digitou",chute)
-
-if (numero_secreto == chute):
+   if (acertou):
     print("Você acertou!")
-else: print("Você errou!")
+   else:
+      if(maior):
+       print("Você errou! O seu chute é maior que o numero secreto.")
+      elif(menor):
+       print("Você errou! O seu chute é menor que o numero secreto.") 
 
+   rodada = rodada + 1
 
-
-
-
+print("Fim do Jogo!")  
